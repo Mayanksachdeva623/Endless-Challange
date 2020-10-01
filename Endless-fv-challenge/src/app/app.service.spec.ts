d954d8bb5edc3fca1dd5ba24d079cfb7f5a3cccc
Mayanksachdeva623 <sachedvamayank@gmail.com>
2020-10-01 17:52:02 +0530

    endless challange complete

---
+++ Endless-fv-challenge/src/app/app.service.spec.ts
@@ -0,0 +1,20 @@
+import { TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { AppService } from './app.service';
+
+describe('AppService', () => {
+  let service: AppService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule
+      ],
+    });
+    service = TestBed.inject(AppService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
